ffababf6eba22e35343df41953dc6b95b1ba807a
callesebbe <carlsebastiangustafsson@gmail.com>
2017-11-14 21:32:01 +0100

    A new beginning

---
+++ client.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+import sys  #for exit
+from random import randint
+#Establishes a UDP-socket
+try:
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+except socket.error, msg:
+    print 'Misslyckades med att skapa socket. Felkod: ' + str(msg[0]) + ' , Felmeddelande : ' + msg[1]
+    sys.exit();
+host = socket.gethostname() # IP of the server change if not localhost!!
+port = 1234
+#-----------------------------
+
+# Just a hacky way for the server to know who has connected
+def tell_server_of_connection():
+    udp_socket.sendto("unneccessary", (host,port))
+
+# Recieve the random generated position from the server
+def recieve_position_and_object_from_server():
+    msg, addr = udp_socket.recvfrom(1024)
+    data = msg.split(',')
+    obj = data[0]
+    coord = data[1],data[2]
+    print obj+' - '+str(coord)
+    return obj, coord
+
+def send_timestamp():
+    udp_socket.sendto("[1,timestamp]", (host,port))
+
+
+def main():
+    tell_server_of_connection()
+    while True:
+        recieve_position_and_object_from_server()
+        send_timestamp()
+    s.close
+if __name__ == "__main__":
+    main()
---
+++ server.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import socket
+import sys  #for exit
+from random import randint
+
+#Get ip address of interface wlp2s0
+#import netifaces as ni
+#ni.ifaddresses('wlp2s0')
+#ip = ni.ifaddresses('wlp2s0')[ni.AF_INET][0]['addr']
+#------------
+
+#Establishes a UDP-socket
+try:
+    udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #UDP
+except socket.error, msg:
+    print 'Misslyckades med att skapa socket. Felkod: ' + str(msg[0]) + ' , Felmeddelande : ' + msg[1]
+    sys.exit();
+host = socket.gethostname() #ip
+port = 1234
+udp_socket.bind((host, port))
+message = []
+address = []
+#-----------------------------
+
+#randomized coordinates for objects
+def randomize_coordinates():
+    obj = (randint(0, 10))
+    x = (randint(0, 100)) #random integer
+    y = (randint(0, 100))
+    return str(obj)+','+str(x)+','+str(y)
+
+#sends position to players from array address
+def send_position_to_players(position, address):
+    # Send postion to players
+    j = 0
+    while j<len(address):
+        try:
+            udp_socket.sendto(position,address[j])
+        except socket.error, msg:
+            print 'Misslyckades med sändning av position. Felkod: '+str(msg[0])+', Felmeddelande: '+msg[1]
+            sys.exit();
+        j = j+1
+    return True
+
+#awaits udp connections from clients and puts them into the global variable address
+def await_connections():
+    i = 0
+    while i<2: # Wait for connection from players
+        m, a = udp_socket.recvfrom(1024)
+        address.append(a)
+        print 'connection from', address[i]
+        i = i+1
+    return True
+
+#Receive timestamp from clients and append to message array
+def recieve_timestamp():
+    i = 0
+    while i<2: # Wait for connection from players
+        m, a = udp_socket.recvfrom(1024)
+        message.append(m)
+        i = i+1
+    return True
+
+
+#Main routine, GameHandler
+def main():
+    #Some variables Needed
+    global address # Address array from connected clients
+    global message # Messages from connected clients, might be redundant.
+    connection_limit = False
+    sent_position = False
+    got_timestamps = False
+
+    print "listening on port, and address" , host, port
+    while True:
+        random_position_and_object = randomize_coordinates()
+        if(not connection_limit):
+            connection_limit = await_connections()
+        if(not sent_position):
+            sent_position = send_position_to_players(random_position_and_object, address)
+            got_timestamps = False
+        if(not got_timestamps):
+            got_timestamps = recieve_timestamp()
+            sent_position = False
+    udp_socket.close()
+
+if __name__ == "__main__":
+    main()
