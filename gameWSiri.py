33b3750a644a2fe6642c528415d1e717cb3b5d10
baurre95 <alexanderbacklund95@gmail.com>
2017-12-05 16:14:24 +0100

    la till en knapp

--- gameWSiri.py
+++ gameWSiri.py
@@ -7,7 +7,6 @@ from client import *
 import time
 
 nrplayers = 2
-points = 0
 player_name = ""
 server= ""
 
@@ -45,30 +44,44 @@ def insideTriangle(left_pt, right_pt, top_pt, click):
     A3 = areaOfTraingle(leftX, leftY, rightX, rightY, click.x, click.y)
     return (A == (A1 + A2 + A3))
 
-def calc_left_point(position):
-    randX = randint(1,500)
-    randY = randint(1,500)
+def calc_left_point(position, width, height):
     xpos = position.getX()
     ypos = position.getY()
-    return Point(xpos - randX,ypos - randY)
+    return Point(xpos - width,ypos - height)
 
-def calc_right_point(position):
-    randX = randint(1,500)
-    randY = randint(1,500)
+def calc_right_point(position, width, height):
     xpos = position.getX()
     ypos = position.getY()
-    return Point(xpos + randX,ypos + randY)
+    return Point(xpos + width,ypos + height)
+
+def drawObjectRec(position, win):
+    left_pt = calc_left_point(position, 150, 50)
+    right_pt = calc_right_point(position, 150, 50)
+    R = Rectangle(left_pt, right_pt)
+    R.setFill("green")
+    R.draw(win)
+    Text(position, "Press to enter our AWESOME game").draw(win)
+    boo = False
+    while(not(boo)):
+        click = win.getMouse() # pause for click in window
+        if(insideRectangle(left_pt, right_pt,click)):
+            boo = True
+        else:
+            print(insideRectangle(left_pt, right_pt, click))
+    R.undraw()
+
 
 def drawObject(position, form_id, win):
     color = ["green", "red", "grey", "white", "black", "orange", "purple"]
     k = randint(1,len(color)-1)
     boo = False
     if form_id == 0:
-        left_pt = calc_left_point(position)
-        right_pt = calc_right_point(position)
+        left_pt = calc_left_point(position, 100,100)
+        right_pt = calc_right_point(position, 100, 100)
         R = Rectangle(left_pt, right_pt)
         R.setFill(color[k])
         R.draw(win)
+        boo = False
         while(not(boo)):
             click = win.getMouse() # pause for click in window
             if(insideRectangle(left_pt, right_pt,click)):
@@ -90,7 +103,7 @@ def drawObject(position, form_id, win):
         C.undraw()
 
     else:
-        rand = randint(1, 300)
+        rand = 150
         leftX = position.getX() - rand
         rightX = position.getX() + rand
         topY = position.getY() - rand
@@ -135,15 +148,17 @@ def make_intro_win():
 
     Text(Point((win.getWidth())/6,win.getHeight()/10), "Player name: ").draw(win)
     name_entry = Entry(Point(win.getWidth()/2,win.getHeight()/10), 50)
-    Text(Point((win.getWidth())/6,9*win.getHeight()/10), "Which server?").draw(win)
-    server_entry = Entry(Point(win.getWidth()/2,9*win.getHeight()/10), 50)
+    Text(Point((win.getWidth())/6,2*win.getHeight()/10), "Which server?").draw(win)
+    server_entry = Entry(Point(win.getWidth()/2,2*win.getHeight()/10), 50)
     name_entry.draw(win)
     server_entry.draw(win)
 
-    win.getMouse()
-    win.close()
+    drawObjectRec(Point(win.getWidth()/2,6*win.getHeight()/10), win)
+
     player_name = name_entry.getText()
     server = server_entry.getText()
+
+    win.close()
     return
 
 def main():
